eda6e91dbf0b35f4883528f0887694c9abb4fc5e
Paulo <pauloh_gl@hotmail.com>
2023-09-28 17:12:36 -0300

    Post Method Tests
    Recebendo dados pelo postman

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
-import { MailService } from './mail/mail.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
 
-@Controller('app')
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly mailService: MailService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
-  @Get('hello')
-  getHello(): string {
-    return this.mailService.sendEmail();
+  @Get()
+  list() {
+    return this.prisma.notification.findMany()
+  }
+
+  @Post()
+  async create(@Body() body: any) {
+    const { recipientId, content, category } = body;
+    console.log(body)
+
+    // return this.prisma.notification.create({
+    //   data: {
+    //     id: randomUUID(),
+    //     content: "Você tem uma nova solicitação de amizade!",
+    //     category: "social",
+    //     recipientId: randomUUID(),
+    //   }
+    // })
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { SMTPMailService } from './mail/smtp-mail-service';
-import { MailService } from './mail/mail.service';
-import { PostMarkMailService } from './mail/postmark-mail-service';
-import { HttpModule } from './http.modules';
+import { HttpModule } from './http.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [AppController],
   providers: [
-    {
-      provide: MailService,
-      useClass: PostMarkMailService
-    }
+    PrismaService
   ],
 })
-export class AppModule {}
+export class AppModule { }
