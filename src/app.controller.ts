eae7723e8818d032cb93daec9f0163ec56e72420
Paulo <pauloh_gl@hotmail.com>
2023-09-28 18:23:58 -0300

    Prisma Implemented

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { randomUUID } from 'node:crypto';
+import { CreateNotificationBody } from './create-notification-body';
 
 @Controller('notifications')
 export class AppController {
@@ -12,9 +13,8 @@ export class AppController {
   }
 
   @Post()
-  async create(@Body() body: any) {
+  async create(@Body() body: CreateNotificationBody) {
     const { recipientId, content, category } = body;
-    console.log(body)
 
     return this.prisma.notification.create({
       data: {
