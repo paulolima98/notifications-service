01eeb56c1587aefe7c52e86898055891a19ce212
Paulo <pauloh_gl@hotmail.com>
2023-09-28 11:15:47 -0300

    Conceito de inversão de dependência
    Fazendo a importação http.module para app.module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SMTPMailService } from './mail/smtp-mail-service';
+import { MailService } from './mail/mail.service';
+import { PostMarkMailService } from './mail/postmark-mail-service';
+import { HttpModule } from './http.modules';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: MailService,
+      useClass: PostMarkMailService
+    }
+  ],
 })
 export class AppModule {}
